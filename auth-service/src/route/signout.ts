7a2049eee9c4ac0fc94fd5cdb0daa6c8dee45175
Shaunak <shaunakprasad@gmail.com>
2023-06-03 21:52:35 -0700

    restructuring tries

--- auth-service/src/route/signout.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.post('/api/auth/signout',(req,res)=>{
-    req.session = null;
-    res.send({message:'Successfully Logout'});
-});
-
-export { router as signOutRouter };
